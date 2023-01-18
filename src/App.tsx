6c4089bb6b5415a1916d0ff0391df270ace74073
Damian Wasilewski <damian.wasilewski@binarapps.com>
2023-01-18 17:56:52 +0100

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
 };
 
 export const Home = () => {
-  return <div>You are in Home page</div>;
+  return <div>You are in Home page, hello</div>;
 };
 export const About = () => {
   return <div>This is the page where you put details about yourself</div>;
